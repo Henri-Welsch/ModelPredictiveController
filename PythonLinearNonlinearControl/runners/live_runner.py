e606670cb84c43d61ea1d926a3c13d943e8d69ec
Henri-Welsch <weljo870@outlook.de>
2026-03-10 11:52:18 +0100

    Refactor LiveRunner: Simplify `run` method and update parameters for streamlined execution

--- PythonLinearNonlinearControl/runners/live_runner.py
+++ PythonLinearNonlinearControl/runners/live_runner.py
@@ -14,7 +14,7 @@ class LiveRunner():
         """
         pass
 
-    def run(self, env, controller, planner):
+    def run(self, controller, planner, curr_x, goal_x):
         """
         Returns:
             history_x (numpy.ndarray): history of the state,
@@ -22,32 +22,12 @@ class LiveRunner():
             history_u (numpy.ndarray): history of the state,
             shape(episode length, input_size)
         """
-        done = False
-        curr_x, info = env.reset()
-        history_x, history_u, history_g = [], [], []
-        step_count = 0
-        score = 0.
+        # plan
+        g_xs = planner.plan(curr_x, goal_x)
 
-        while not done:
-            logger.debug("Step = {}".format(step_count))
-            # plan
-            g_xs = planner.plan(curr_x, info["goal_state"])
+        # obtain sol
+        u = controller.obtain_sol(curr_x, g_xs)
 
-            # obtain sol
-            u = controller.obtain_sol(curr_x, g_xs)
-
-            # step
-            next_x, cost, done, info = env.step(u)
-
-            # save
-            history_u.append(u)
-            history_x.append(curr_x)
-            history_g.append(g_xs[0])
-            # update
-            curr_x = next_x
-            score += cost
-            step_count += 1
-
-        logger.debug("Controller type = {}, Score = {}"
-                     .format(controller, score))
-        return np.array(history_x), np.array(history_u), np.array(history_g)
+        message = "Controller = {}, Goal = {}, Actions = {}"
+        logger.debug(message.format(controller, goal_x, u))
+        return u
