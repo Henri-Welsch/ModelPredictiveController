dda82263f9244eb643c5c918d19b2764124b2eac
Henri-Welsch <weljo870@outlook.de>
2026-03-10 13:19:00 +0100

    Refactor LiveRunner: Update `__init__` to accept controller and planner, simplify `run` method parameters and logic

--- PythonLinearNonlinearControl/runners/live_runner.py
+++ PythonLinearNonlinearControl/runners/live_runner.py
@@ -9,25 +9,15 @@ class LiveRunner():
     """ live runner
     """
 
-    def __init__(self):
-        """
-        """
-        pass
+    def __init__(self, controller, planner):
+        self.controller = controller
+        self.planner = planner
 
-    def run(self, controller, planner, curr_x, goal_x):
-        """
-        Returns:
-            history_x (numpy.ndarray): history of the state,
-            shape(episode length, state_size)
-            history_u (numpy.ndarray): history of the state,
-            shape(episode length, input_size)
-        """
-        # plan
-        g_xs = planner.plan(curr_x, goal_x)
-
-        # obtain sol
-        u = controller.obtain_sol(curr_x, g_xs)
+    def run(self, curr_x, goal_x):
+        g_xs = self.planner.plan(curr_x, goal_x)
+        u = self.controller.obtain_sol(curr_x, g_xs)
 
         message = "Controller = {}, Goal = {}, Actions = {}"
-        logger.debug(message.format(controller, goal_x, u))
+        logger.debug(message.format(self.controller, goal_x, u))
+
         return u
